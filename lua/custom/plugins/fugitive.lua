f4c1d7e057b9bf1790cf9cb2b877b1cd2e89bc5d
OLogizO <38345570+OLogizO@users.noreply.github.com>
2025-08-27 16:05:48 +0200

    added fugitive to easier handle git, with some commands

---
+++ lua/custom/plugins/fugitive.lua
@@ -0,0 +1,9 @@
+return {
+  'tpope/vim-fugitive',
+  config = function()
+    vim.keymap.set('n', '<leader>gs', ':Git<CR>', { desc = 'Git: git [s]atus' })
+    vim.keymap.set('n', '<leader>gd', ':Gvdiffsplit<CR>', { desc = 'Git: git vertical [d]iff split' })
+    vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { desc = 'Git: git [b]lame' })
+    vim.keymap.set('v', '<leader>gdo', ':diffput<CR>', { desc = 'Git: stage selected changes in diff window' })
+  end,
+}

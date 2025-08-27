ef9cb528bb0e77bcb56ebe1051eeff3521f1826e
OLogizO <38345570+OLogizO@users.noreply.github.com>
2025-08-27 16:06:19 +0200

    added this to fix the indent problem i had in abernalia

--- lua/options.lua
+++ lua/options.lua
@@ -9,6 +9,11 @@ vim.o.number = true
 --  Experiment for yourself to see if you like it!
 vim.o.relativenumber = true
 
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 

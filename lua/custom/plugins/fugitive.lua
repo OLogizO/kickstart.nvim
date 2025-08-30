9c11684fbebfb654818b7f0f6a767ac758943b38
OLogizO <38345570+OLogizO@users.noreply.github.com>
2025-08-31 00:20:12 +0200

    added rebase shortcuts.

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -6,5 +6,7 @@ return {
     vim.keymap.set('n', '<leader>gb', ':Git blame<CR>', { desc = 'Git: git [b]lame' })
     vim.keymap.set('v', '<leader>gdo', ':diffput<CR>', { desc = 'Git: stage selected changes in diff window' })
     vim.keymap.set('n', '<leader>gm', ':Gvdiffsplit!<CR>', { desc = 'Git: solve [m]erge conflict. open a 3 way split.' })
+    vim.keymap.set('n', '<leader>gir', ':Git rebase -i HEAD~', { desc = 'Git: [I]nteractive [R]ebase' })
+    vim.keymap.set('n', '<leader>gic', ':Git rebase --continue', { desc = 'Git: [I]nteractive [C]ontinue' })
   end,
 }

405c709c849a3ecbd0988b67ee5cea04d9feca8e
OLogizO <38345570+OLogizO@users.noreply.github.com>
2025-09-01 00:23:58 +0200

    seems like i forgot about scopes. the variable v.count is not available in the deferfunction since it is being called the next frame? or whatever i should call it.

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -7,11 +7,11 @@ return {
     vim.keymap.set('v', '<leader>gdo', ':diffput<CR>', { desc = 'Git: stage selected changes in diff window' })
     vim.keymap.set('n', '<leader>gm', ':Gvdiffsplit!<CR>', { desc = 'Git: solve [m]erge conflict. open a 3 way split.' })
     vim.keymap.set('n', '<leader>gir', function()
+      local count = vim.v.count
+      if count == 0 then
+        count = 1
+      end
       vim.defer_fn(function()
-        local count = vim.v.count
-        if count == 0 then
-          count = 1
-        end
         vim.cmd('Git rebase -i HEAD~' .. count)
       end, 0)
     end, { desc = 'Git: [I]nteractive [R]ebase', expr = true })
